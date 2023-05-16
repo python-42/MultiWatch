bd3b44e527f5a0225cd1605c1e84a6bb6391346a
python-42 <herrmann.jake@protonmail.com>
2023-05-16 18:52:48 -0500

    Created back arrow and renamed fragment

--- app/src/main/java/rrhs/track/multiwatch/TimerViewFragment.java
+++ app/src/main/java/rrhs/track/multiwatch/TimerViewFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import rrhs.track.multiwatch.databinding.FragmentSecondBinding;
+import rrhs.track.multiwatch.databinding.FragmentTimerviewBinding;
 
-public class SecondFragment extends Fragment {
+public class TimerViewFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private FragmentTimerviewBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentTimerviewBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,10 +29,10 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
+                NavHostFragment.findNavController(TimerViewFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
