0dfc379de64ecdc0b64100c8008bd76657811dfe
Jake <herrmann.jake@protonmail.com>
2023-05-20 09:01:32 -0500

    Basic timer view is fully working, added app state var for timer name

--- app/src/main/java/rrhs/track/multiwatch/AppState.java
+++ app/src/main/java/rrhs/track/multiwatch/AppState.java
@@ -2,7 +2,9 @@ package rrhs.track.multiwatch;
 
 public class AppState {
     private final TimerService timerService;
+    private String desiredTimerName;
     private static AppState instance;
+
 
     private AppState() {
         timerService = new TimerService(400);
@@ -18,4 +20,9 @@ public class AppState {
     public TimerService getTimerService() {
         return timerService;
     }
+
+    public void setDesiredTimerName(String desiredTimerName) {
+        this.desiredTimerName = desiredTimerName;
+    }
+    public String getDesiredTimerName() {return desiredTimerName;}
 }
--- app/src/main/java/rrhs/track/multiwatch/TimerViewFragment.java
+++ app/src/main/java/rrhs/track/multiwatch/TimerViewFragment.java
@@ -6,22 +6,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import rrhs.track.multiwatch.databinding.FragmentTimerviewBinding;
+import rrhs.track.multiwatch.time.Timer;
 
 public class TimerViewFragment extends Fragment {
 
     private FragmentTimerviewBinding binding;
+    private TimerService service;
+    private Timer timer;
+
+    private boolean timerRunning = false;
 
     @Override
     public View onCreateView(
+
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentTimerviewBinding.inflate(inflater, container, false);
+
+        service = AppState.getInstance().getTimerService();
+        String timerName = AppState.getInstance().getDesiredTimerName();
+        if(!service.timerExists(timerName)) {
+            service.addTimer(timerName);
+        }
+        timer = service.getTimer(timerName);
+        binding.timerName.setText(timerName);
+
         return binding.getRoot();
 
     }
@@ -29,19 +43,55 @@ public class TimerViewFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.backButton.setOnClickListener(new View.OnClickListener() {
+        binding.lapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(TimerViewFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                timer.lap();
             }
         });
+
+        binding.reset.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 timer.reset();
+             }
+         });
+
+        binding.startStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(timer.isTimerRunning()) {
+                    timer.stopTimer();
+                }else {
+                    timer.startTimer();
+                }
+            }
+        });
+
+        service.startUpdateThread(() -> updateData());
+    }
+
+    public void updateData() {
+        binding.timerTime.setText(timer.getElapsedTime().toString());
+        binding.halfPace.setText(timer.halfMilePace().toString());
+        binding.milePace.setText(timer.milePace().toString());
+        binding.twoPace.setText(timer.twoMilePace().toString());
+
+        if(timerRunning != timer.isTimerRunning()) {
+            if(timer.isTimerRunning()) {
+                binding.startStop.setImageDrawable(ContextCompat.getDrawable(binding.getRoot().getContext(), R.drawable.baseline_pause_circle_outline_24));
+            }else {
+                binding.startStop.setImageDrawable(ContextCompat.getDrawable(binding.getRoot().getContext(), R.drawable.baseline_play_circle_outline_24));
+            }
+        }
+        timerRunning = timer.isTimerRunning();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        service.stopWorker();
+        //binding = null;
     }
 
 }
